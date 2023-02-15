970c477fdb0c7f4c7edaf14349356d8e37328c78
Md. Millennium Rahman <69154960+millennium58@users.noreply.github.com>
2023-02-15 12:56:27 +0600

    First Commit

---
+++ VideoEditorPractice/src/videoeditorpractice/VideoEditorPractice.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package videoeditorpractice;
+
+/**
+ *
+ * @author phoenix
+ */
+public class VideoEditorPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
